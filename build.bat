61a02a956aecb0f6a89874c2c0a4b8d50860c1d2
Erwin <erwin.wpgb@bk3.nl>
2026-02-16 12:44:47 +0100

    build.bat gefixed

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-for /f "tokens=*" %%i in ('git describe') do set VERSION=%%i
+for /f "tokens=*" %%i in ('git describe --tags') do set VERSION=%%i
 rem echo "building WerkplekGebondenPrinter-%VERSION%.zip"
 msbuild WerkplekGebondenPrinter.csproj /t:build /p:Version=%VERSION:~1% /p:Configuration=Release /p:DebugSymbols=false /p:DebugType=None /p:OutputPath=build\WerkplekGebondenPrinter-%VERSION%
-rem powershell -Command "Compress-Archive -Path build\WerkplekGebondenPrinter-%VERSION%\* -DestinationPath build\WerkplekGebondenPrinter-%VERSION%.zip"
+powershell -Command "Compress-Archive -Path build\WerkplekGebondenPrinter-%VERSION%\* -DestinationPath build\WerkplekGebondenPrinter-%VERSION%.zip"
